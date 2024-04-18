1be0a7106c0e4ec9eb0be7363f3dcb5b42678a63
boris rafael beker <borisrafaelbeker@gmail.com>
2024-04-18 16:38:36 -0300

    mejoras

--- src/components/CarruselComp/CarruselComp.jsx
+++ src/components/CarruselComp/CarruselComp.jsx
@@ -1,3 +1,4 @@
+import './CarruselComp.css'
 import { useEffect, useState } from "react"
 
 export default function CarruselComp({ images }) {
--- src/components/HomeComponent.jsx
+++ src/components/HomeComponent.jsx
@@ -1,7 +1,7 @@
 import image1 from '../assets/image1.webp'
 import image2 from '../assets/image2.jpg'
 import image3 from '../assets/image3.jpg'
-import CarruselComp from './CarruselComp'
+import CarruselComp from './CarruselComp/CarruselComp.jsx'
 import ProductsComp from './ProductsComp'
 
 export default function HomeComponent(){
